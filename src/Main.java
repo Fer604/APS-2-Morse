e90d0e5c2b88aaed6aed9abaa768cfe999f83cf1
andrééé <andreestevesarantes19@gmail.com>
2025-10-22 22:59:34 -0300

    fix: formated main

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void menu(ArvoreBinare arvre,Scanner scan){
+    public static void menu(ArvoreBinare arvore,Scanner scan){
         System.out.println("---MENU---");
         System.out.println("1. Inserir");
         System.out.println("2. Buscar");
         System.out.println("3. Remover");
         System.out.println("0. Sair");
         int opcao = scan.nextInt();
+        
         switch(opcao){
             case 1:
                 System.out.println("Digite o caracter:");
                 char caracter = scan.next().charAt(0);
                 System.out.println("Digite o codigo:");
                 String codigo = scan.next();
-                arvre.inserir(codigo,caracter);
+                arvore.inserir(codigo,caracter);
                 break;
+            
             case 2:
                 System.out.println("Digite o codigo:");
                 String codigo2 = scan.next();
-                arvre.buscar(codigo2);
+                arvore.buscar(codigo2);
                 break;
+            
             case 3:
                 System.out.println("Digite o codigo:");
                 String codigo3 = scan.next();
-                arvre.remover(codigo3);
+                arvore.remover(codigo3);
                 break;
-            case 0:
+            
+            default:
                 return;
         }
-        menu(arvre,scan);
+        menu(arvore,scan);
     }
     public static void main(String[] args) {
         //teste
         ArvoreBinare arvore = new ArvoreBinare();
-
 
         arvore.inserir(".-", 'A');
         arvore.inserir("-...", 'B');
@@ -62,7 +65,6 @@ public class Main {
         arvore.inserir("-..-", 'X');
         arvore.inserir("-.--", 'Y');
         arvore.inserir("--..", 'Z');
-
 
         Scanner scan = new Scanner(System.in);
         menu(arvore,scan);
